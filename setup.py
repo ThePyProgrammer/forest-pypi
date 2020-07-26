42d95a1117209bf80585c1e04bb0c41c52ad7534
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2020-07-26 13:24:13 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
     
 setup(
     name="forest", # Replace with your own username
-    version="0.0.1",
+    version="1.0",
     author="ThePyProgrammer",
     author_email="prannayagupta@gmail.com",
     description="A data science and machine learning library made by me using codes found online. It is based on the pyforest module.",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    license="MIT",
     url="https://github.com/ThePyProgrammer/forest",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
+    install_requires=['pandas', 'numpy', 'np'], #external packages as dependencies
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
